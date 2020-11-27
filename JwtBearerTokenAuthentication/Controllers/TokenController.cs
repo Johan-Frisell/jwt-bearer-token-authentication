19bcc5200e4092983bd80364f89e87f0b041018f
Johan Frisell <johan.frisell@svea.com>
2020-11-27 08:47:24 +0100

    Added TokenController

---
+++ JwtBearerTokenAuthentication/Controllers/TokenController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JwtBearerTokenAuthentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : ControllerBase
+    {
+        [HttpGet("/Token")]
+        public IActionResult GenerateToken()
+        {
+            return Ok("TOKEN");
+        }
+    }
+}

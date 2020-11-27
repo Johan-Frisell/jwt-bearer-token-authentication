cc268ab9546fd8110907d4d0ec26cd11564ea1a8
Johan Frisell <johan.frisell@svea.com>
2020-11-27 09:22:35 +0100

    Added CarController

---
+++ JwtBearerTokenAuthentication/Controllers/CarController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace JwtBearerTokenAuthentication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        [HttpGet("/Cars")]
+        public IActionResult Index()
+        {
+            var cars = new List<string>() { "BMW", "OPEL", "KIA" };
+
+            return Ok(JsonConvert.SerializeObject(cars));
+        }
+    }
+}

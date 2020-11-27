0b3d4718755238749ab7091d84c499b04aa8f3e6
Johan Frisell <johan.frisell@svea.com>
2020-11-27 09:16:36 +0100

    Implemented GenerateToken-method

--- JwtBearerTokenAuthentication/Controllers/TokenController.cs
+++ JwtBearerTokenAuthentication/Controllers/TokenController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace JwtBearerTokenAuthentication.Controllers
 {
@@ -9,7 +15,35 @@ namespace JwtBearerTokenAuthentication.Controllers
         [HttpGet("/Token")]
         public IActionResult GenerateToken()
         {
-            return Ok("TOKEN");
+            SymmetricSecurityKey key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Guid.NewGuid().ToString()));
+            SigningCredentials creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var subject = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, "ClaimTypes.NameIdentifier"),
+                new Claim(ClaimTypes.Name, "ClaimTypes.Name"),
+                new Claim(ClaimTypes.Email, "ClaimTypes.Email"),
+                new Claim(ClaimTypes.MobilePhone, "ClaimTypes.MobilePhone"),
+            };
+
+            var claims = new Dictionary<string, object>();
+            claims.Add("Firstname", "Firstname_test");
+            claims.Add("Lastname", "Lastname_test")
+;
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Claims = claims,
+                Subject = new ClaimsIdentity(subject),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds,
+                Issuer = "localhost",
+                Audience = "user"
+            };
+
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(tokenHandler.WriteToken(token));
         }
     }
 }

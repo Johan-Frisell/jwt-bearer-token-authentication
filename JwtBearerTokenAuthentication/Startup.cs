bcb8482d82c4db4bcfeb570882cd27d824b7d0e1
Johan Frisell <johan.frisell@svea.com>
2020-11-27 08:43:18 +0100

    Added Swagger

--- JwtBearerTokenAuthentication/Startup.cs
+++ JwtBearerTokenAuthentication/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace JwtBearerTokenAuthentication
 {
@@ -25,6 +20,34 @@ namespace JwtBearerTokenAuthentication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(
+               swagger =>
+               {
+                   swagger.AddSecurityDefinition("Authorization", new OpenApiSecurityScheme
+                   {
+                       Description = "For accessing the API a valid JWT token must be passed in all the queries in the 'Authorization' header.",
+                       Name = "Authorization",
+                       In = ParameterLocation.Header,
+                       Type = SecuritySchemeType.ApiKey,
+                       Scheme = "Bearer"
+                   });
+
+                   swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                   {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+                   });
+               });
+
             services.AddControllers();
         }
 
@@ -35,6 +58,16 @@ namespace JwtBearerTokenAuthentication
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Svea.SalesFinance.Payment.API");
+            });
 
             app.UseHttpsRedirection();
 
